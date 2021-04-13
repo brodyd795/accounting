7cd2952f981fe9a8069ce297b4a2becd7561c9b1
Brody Dingel <brodydingel795@gmail.com>
2021-04-13 07:11:19 -0500

    Add log to controller

--- pages/api/controllers/home.js
+++ pages/api/controllers/home.js
@@ -1,6 +1,8 @@
 import {homeService} from '../services/home-service';
 
 export default async (req, res) => {
+	const database = process.env.VERCEL_GIT_COMMIT_REF === 'main' ? process.env.DB_NAME : process.env.DEV_DB_NAME;
+	console.log(`database`, database)
 	const data = await homeService();
 
 	res.json(data);
