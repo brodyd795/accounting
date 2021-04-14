3f5bce460fd988f89781d2e6f40cf5c1ab5842a4
Brody Dingel <brodydingel795@gmail.com>
2021-04-14 06:38:59 -0500

    Update config redirect urls

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,11 +1,7 @@
 import auth0 from '../../../lib/auth0';
 
-import config from '../../../lib/config';
-
 export default async (req, res) => {
 	try {
-		console.log(`config`, config)
-		console.log('in login');
 		await auth0.handleLogin(req, res);
 	} catch (error) {
 		res.status(error.status || 500).end(error.message);
