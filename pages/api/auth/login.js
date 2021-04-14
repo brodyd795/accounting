cfb96ec8219154359bcad827da56bf2f43d712fa
Brody Dingel <brodydingel795@gmail.com>
2021-04-13 22:18:03 -0500

    Logging

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -2,7 +2,7 @@ import auth0 from '../../../lib/auth0';
 
 export default async (req, res) => {
 	try {
-		console.log('in login', req);
+		console.log('in login');
 		await auth0.handleLogin(req, res);
 	} catch (error) {
 		res.status(error.status || 500).end(error.message);
