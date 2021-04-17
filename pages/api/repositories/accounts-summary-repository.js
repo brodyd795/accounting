14f4c0538aa66a7b46b768fe2ec2a8a644831b76
Brody Dingel <brodydingel795@gmail.com>
2021-04-16 21:11:38 -0500

    Fill out repo

--- pages/api/repositories/accounts-summary-repository.js
+++ pages/api/repositories/accounts-summary-repository.js
@@ -2,15 +2,22 @@ import escape from 'sql-template-strings';
 
 import {conn} from './transaction-wrapper-repository';
 
-export const accountsSummaryRepository = async () => {
+export const accountsSummaryRepository = async ({date}) => {
 	return conn().query(
 		escape`
-			SELECT
-                *
+            SELECT
+                balances.balance,
+                accounts.accountName,
+                accounts.category,
+                accounts.accountId
             FROM
-                transactions
-            LIMIT
-                1
+                balances
+            INNER JOIN
+                accounts
+            ON
+                balances.accountId = accounts.accountId
+            WHERE
+                balances.date = ${date}
 		`
 	);
 };
