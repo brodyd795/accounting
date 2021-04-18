bacc6e547029c2d49c18e6c368b5345ff67890eb
Brody Dingel <brodydingel795@gmail.com>
2021-04-18 15:16:04 -0500

    Calculate start and end dates for repo

--- __TESTS__/pages/api/services/accounts-summary-service.spec.js
+++ __TESTS__/pages/api/services/accounts-summary-service.spec.js
@@ -1,23 +1,34 @@
 import Chance from 'chance';
+import startOfMonth from 'date-fns/startOfMonth';
+import addMonths from 'date-fns/addMonths';
 
 import {accountsSummaryRepository} from '../../../../pages/api/repositories/accounts-summary-repository';
 import {withTransactionWrapper} from '../../../../pages/api/repositories/transaction-wrapper-repository';
 import {accountsSummaryService} from '../../../../pages/api/services/accounts-summary-service';
 
+
 jest.mock('../../../../pages/api/repositories/accounts-summary-repository');
 jest.mock('../../../../pages/api/repositories/transaction-wrapper-repository');
+jest.mock('date-fns/startOfMonth');
+jest.mock('date-fns/addMonths');
 
 const chance = new Chance();
 
 describe('accounts-summary-service', () => {
-    let expectedProps, expectedResults;
+    let expectedProps, expectedResults, expectedStartDate, expectedEndDate;
 
     beforeEach(() => {
-        expectedProps = {};
+        expectedProps = {
+            date: chance.date()
+        };
         expectedResults = chance.word();
+        expectedStartDate = chance.date();
+        expectedEndDate = chance.date();
 
         accountsSummaryRepository.mockResolvedValue(expectedResults);
         withTransactionWrapper.mockImplementation(async (fn, props) => fn(props));
+        startOfMonth.mockReturnValue(expectedStartDate);
+        addMonths.mockReturnValue(expectedEndDate);
     });
 
     afterEach(() => {
@@ -31,11 +42,23 @@ describe('accounts-summary-service', () => {
         expect(withTransactionWrapper).toHaveBeenCalledWith(expect.anything(), expectedProps);
     });
 
+    test('should calculate start and end dates', async () => {
+        await accountsSummaryService(expectedProps);
+
+        expect(startOfMonth).toHaveBeenCalledTimes(1);
+        expect(startOfMonth).toHaveBeenCalledWith(expectedProps.date);
+        expect(addMonths).toHaveBeenCalledTimes(1);
+        expect(addMonths).toHaveBeenCalledWith(expectedStartDate, 1);
+    });
+
     test('should call repo', async () => {
         await accountsSummaryService(expectedProps);
 
         expect(accountsSummaryRepository).toHaveBeenCalledTimes(1);
-        expect(accountsSummaryRepository).toHaveBeenCalledWith(expectedProps);
+        expect(accountsSummaryRepository).toHaveBeenCalledWith({
+            endDate: expectedEndDate,
+            startDate: expectedStartDate
+        });
     });
 
     test('should return results', async () => {
