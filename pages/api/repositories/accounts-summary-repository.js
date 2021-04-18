47d3824fdbb6338e20d9f269242b304a1430ed6d
Brody Dingel <brodydingel795@gmail.com>
2021-04-18 14:57:25 -0500

    Add query to select account balances

--- pages/api/repositories/accounts-summary-repository.js
+++ pages/api/repositories/accounts-summary-repository.js
@@ -6,17 +6,80 @@ export const accountsSummaryRepository = ({date}) =>
     conn().query(
         escape`
             SELECT
-                balances.balance,
+                accounts.accountId,
+                IFNULL(s.amount, 0) AS balance,
                 accounts.accountName,
-                accounts.category,
-                accounts.accountId
+                accounts.category
             FROM
-                balances
-            INNER JOIN
                 accounts
-            ON
-                balances.accountId = accounts.accountId
-            WHERE
-                balances.date = ${date}
+            LEFT JOIN (
+                SELECT
+                    accountid,
+                    SUM(amount) AS amount
+                FROM (
+                    SELECT
+                        fromaccountid AS accountId,
+                        amount * -1.00 AS amount,
+                        accountName,
+                        category
+                    FROM
+                        transactions
+                    INNER JOIN
+                        accounts
+                    ON
+                        accounts.accountid = transactions.fromaccountid
+                    WHERE
+                        DATE >= IF
+                            (
+                                accounts.category = 'Income'
+                                OR
+                                accounts.category = 'Expenses',
+                                '2021-02-01',
+                                '2000-01-01'
+                            )
+                    AND 
+                        DATE < IF
+                            (
+                                accounts.category = 'Income'
+                                OR
+                                accounts.category = 'Expenses',
+                                '2021-03-01',
+                                '3000-01-01'
+                            )
+                    UNION ALL
+                        SELECT
+                            toaccountid,
+                            amount,
+                            accountName,
+                            category
+                    FROM
+                        transactions
+                    INNER JOIN
+                        accounts
+                    ON
+                        accounts.accountid = transactions.toaccountid
+                    WHERE  DATE >= IF
+                        (
+                            accounts.category = 'Income'
+                            OR 
+                            accounts.category = 'Expenses',
+                            '2021-02-01',
+                            '2000-01-01'
+                        )
+                    AND DATE < IF
+                        (
+                            accounts.category = 'Income'
+                            OR 
+                            accounts.category = 'Expenses',
+                            '2021-03-01',
+                            '3000-01-01'
+                        )
+                    ) t
+                GROUP BY
+                    accountId,
+                    accountName,
+                    category
+                ) s
+            ON accounts.accountId = s.accountid; 
 		`
     );
