c2e2e6cb980ac4da6f1b84bdab554df1b4db5f3c
Brody Dingel <brodydingel795@gmail.com>
2021-04-22 06:22:44 -0500

    Make call for data, add basic loading and error, display initial data

--- components/home/index.js
+++ components/home/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import useSWR from 'swr';
 
 import {Layout} from "../layout";
+import fetch from '../../lib/fetch';
 
-export const Home = () => (
-    <Layout>
-        <div>{'Home here'}</div>
-    </Layout>
-);
+export const Home = () => {
+    const { data, error } = useSWR('/api/controllers/accounts-summary-controller', fetch);
+    
+    if (error) {
+        return <div>{'Error!'}</div>
+    }
+
+    if (!data) {
+        return <div>{'Loading...'}</div>
+    }
+
+    return (
+        <Layout>
+            <div>{'Home here'}</div>
+            <div>
+                {data.map((account) => <div key={account.accountId}>
+                    {account.balance}
+                </div>)}
+            </div>
+        </Layout>
+    )
+};
