3a26cc6485288a17df611ff8027d50273d13ced4
Brody Dingel <brodydingel795@gmail.com>
2021-04-24 06:42:36 -0500

    Delete unneeded auth files

--- pages/api/auth/callback.js
+++
@@ -1,9 +0,0 @@
-import auth0 from '../../../lib/auth0';
-
-export default async (req, res) => {
-    try {
-        await auth0.handleCallback(req, res);
-    } catch (error) {
-        res.status(error.status || 500).end(error.message);
-    }
-};
