630c59be805eae6c5d193abc5f3b8b0482fa94be
Brody Dingel <brodydingel795@gmail.com>
2021-04-24 06:43:51 -0500

    Add auth to api

--- pages/api/controllers/accounts-summary-controller.js
+++ pages/api/controllers/accounts-summary-controller.js
@@ -1,13 +1,23 @@
+import {withApiAuthRequired, getSession} from '@auth0/nextjs-auth0';
+
+import {ADMIN_EMAILS} from '../../../enums/admin-emails';
 import {accountsSummaryService} from '../services/accounts-summary-service';
 
-export default async (req, res) => {
+export default withApiAuthRequired(async (req, res) => {
     try {
+        const session = getSession(req, res);
+
+        if (!ADMIN_EMAILS.includes(session.user.email)) {
+            throw new Error('Unauthorized.');
+        }
+
         const date = req.body.date;
 
         const data = await accountsSummaryService({date});
 
         res.json(data);
     } catch (error) {
+        console.log(`error`, error)
         res.status(error.status || 500).end(error.message);
     }
-};
+});
