7d19660e8bcf617a6a0ba1f47a2709078425d406
Brody Dingel <brodydingel795@gmail.com>
2021-04-24 14:31:27 -0500

    Add auth to home page

--- pages/index.js
+++ pages/index.js
@@ -1,76 +1,27 @@
 import React from 'react';
-import Link from 'next/link';
 import {useUser, withPageAuthRequired} from '@auth0/nextjs-auth0';
 
-const Home = () => {
-    const { user, error, isLoading } = useUser();
-    console.log(`user`, user)
+import {ADMIN_EMAILS} from '../enums/admin-emails';
+import {Home} from '../components/home';
 
-    return (
-        <div>
-                    <header>
-            <nav>
-                <ul>
-                <li>
-                    <Link href="/">
-                    <a>Home</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/protected-page">
-                    <a>Protected Page</a>
-                    </Link>
-                </li>
-                {user ? (
-                    <>
-                    <li>
-                        <a href="/api/auth/logout" data-testid="logout">
-                        Logout
-                        </a>
-                    </li>
-                    </>
-                ) : (
-                    <>
-                    <li>
-                        <a href="/api/auth/login" data-testid="login">
-                        Login
-                        </a>
-                    </li>
-                    </>
-                )}
-                </ul>
-            </nav>
-            </header>
-            {isLoading && <p>Loading login info...</p>}
+const Index = () => {
+    const {user, error, isLoading} = useUser();
 
-            {error && (
-            <>
-                <h4>Error</h4>
-                <pre>{error.message}</pre>
-            </>
-            )}
-    
-            {user && (
-            <>
-                <h4>Rendered user info on the client</h4>
-                <pre data-testid="profile">{JSON.stringify(user, null, 2)}</pre>
-            </>
-            )}
-    
-            {!isLoading && !error && !user && (
-            <>
-                <p>
-                To test the login click in <i>Login</i>
-                </p>
-                <p>
-                Once you have logged in you should be able to click in <i>Protected Page</i> and <i>Logout</i>
-                </p>
-            </>
-            )}
-        </div>
-    );
+    if (error) {
+        return 'Error';
+    }
+
+    if (isLoading) {
+        return 'Loading user profile...';
+    }
+
+    if (!user || !user.email || !ADMIN_EMAILS.includes(user.email)) {
+        return 'Unauthorized';
+    }
+
+    return <Home />
 };
 
 export const getServerSideProps = withPageAuthRequired();
 
-export default Home;
+export default Index;
