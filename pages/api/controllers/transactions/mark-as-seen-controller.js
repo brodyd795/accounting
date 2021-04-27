9f3fb77b0d56f5b4a231dd005febda84effdc46d
Brody Dingel <brodydingel795@gmail.com>
2021-04-27 06:18:28 -0500

    Add api for mark-as-seen

---
+++ pages/api/controllers/transactions/mark-as-seen-controller.js
@@ -0,0 +1,30 @@
+import {withApiAuthRequired, getSession} from '@auth0/nextjs-auth0';
+import * as Sentry from '@sentry/node'
+
+import {ADMIN_EMAILS} from '../../../../enums/admin-emails';
+import {markAsSeenService} from '../../services/mark-as-seen-service';
+import {init} from '../../../../utils/sentry';
+
+init()
+
+export default withApiAuthRequired(async (req, res) => {
+    try {
+        const session = getSession(req, res);
+
+        if (!ADMIN_EMAILS.includes(session.user.email)) {
+            throw new Error('Unauthorized.');
+        }
+
+        const transactionId = req.body.transactionId;
+
+        const data = await markAsSeenService({transactionId});
+        console.log(`data`, data)
+
+        res.json(data);
+    } catch (error) {
+        console.log(`error`, error)
+
+        Sentry.captureException(error);
+        res.status(error.status || 500).end(error.message);
+    }
+});
