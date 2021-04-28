61e4bf81d2fa3ccc2e3326bd96637f1f9be6442d
Brody Dingel <brodydingel795@gmail.com>
2021-04-28 06:31:32 -0500

    Update accounts list service to be ready for select

--- pages/api/services/accounts-list-service.js
+++ pages/api/services/accounts-list-service.js
@@ -1,10 +1,22 @@
 import {withTransactionWrapper} from '../repositories/transaction-wrapper-repository';
 import {accountsListRepository} from '../repositories/accounts-list-repository';
+import {transactionCategories} from '../../../enums/transaction-categories';
 
 const accountsList = async () => {
     const data = await accountsListRepository();
 
-    return data;
+    const options = Object.values(transactionCategories).map((category) => ({
+		label: category,
+		options: data
+			.filter((account) => account.category === category)
+			.map((account) => ({
+				accountId: account.accountId,
+				label: account.accountName.replace(/_/g, " "),
+				value: account.accountName
+			}))
+	}));
+
+    return options;
 };
 
 export const accountsListService = (props) => withTransactionWrapper(accountsList, props);
