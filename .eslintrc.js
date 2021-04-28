863de2fc337816712eff5a5147a18327644e3ea5
Brody Dingel <brodydingel795@gmail.com>
2021-04-28 05:55:11 -0500

    Update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     extends: 'get-off-my-lawn',
     rules: {
         'import/group-exports': 0,
+        'react/jsx-curly-brace-presence': 0,
         'unicorn/no-null': 0
     }
 };
