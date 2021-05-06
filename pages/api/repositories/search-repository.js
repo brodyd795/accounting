4d2eca95bcbb3d3a0b111aed4b7b81cfd1e6dbc4
Brody Dingel <38290629+brodyd795@users.noreply.github.com>
2021-05-06 06:00:03 -0500

    Add search results!

--- pages/api/repositories/search-repository.js
+++ pages/api/repositories/search-repository.js
@@ -12,9 +12,27 @@ export const searchRepository = ({
     conn().query(
         `
             SELECT
-                *
+                transactionId,
+                date,
+                fromAccountId,
+                toAccountId,
+                accountsTo.accountName fromAccountName,
+                accountsTo.category fromAccountCategory,
+                accountsFrom.accountName toAccountName,
+                accountsFrom.category toAccountCategory,
+                amount,
+                comment,
+                isMarkedAsSeen
             FROM
                 transactions
+            INNER JOIN
+                accounts accountsTo
+            ON
+                accountsTo.accountId = transactions.fromAccountId
+            INNER JOIN
+                accounts accountsFrom
+            ON
+                accountsFrom.accountId = transactions.toAccountId
             WHERE
                 comment like ?
             AND
@@ -29,6 +47,9 @@ export const searchRepository = ({
                 amount <= ?
             AND
                 date <= ?
+            ORDER BY
+                date
+            DESC
 		`,
         [
             comment,
