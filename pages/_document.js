482b131dae5b85f8a76314d1843a5c0badc0c7d4
Brody Dingel <brodydingel795@gmail.com>
2021-09-14 20:08:23 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Document, {Html, Head, Main, NextScript} from 'next/document';
 
-const APP_NAME = 'next-pwa example';
-const APP_DESCRIPTION = 'This is an example of using next-pwa plugin';
+const APP_NAME = 'Dingel accounting';
+const APP_DESCRIPTION = "Our family's personal accounting system";
 
 export default class extends Document {
     static getInitialProps(ctx) {
@@ -11,33 +11,24 @@ export default class extends Document {
 
     render() {
         return (
-            <Html lang="en" dir="ltr">
+            <Html dir="ltr" lang="en">
                 <Head>
-                    <meta name="application-name" content={APP_NAME} />
-                    <meta name="apple-mobile-web-app-capable" content="yes" />
-                    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-                    <meta name="apple-mobile-web-app-title" content={APP_NAME} />
-                    <meta name="description" content={APP_DESCRIPTION} />
-                    <meta name="format-detection" content="telephone=no" />
-                    <meta name="mobile-web-app-capable" content="yes" />
-                    <meta name="theme-color" content="#FFFFFF" />
-                    {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
-                    {/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' /> */}
+                    <meta content={APP_NAME} name="application-name" />
+                    <meta content="yes" name="apple-mobile-web-app-capable" />
+                    <meta content="default" name="apple-mobile-web-app-status-bar-style" />
+                    <meta content={APP_NAME} name="apple-mobile-web-app-title" />
+                    <meta content={APP_DESCRIPTION} name="description" />
+                    <meta content="telephone=no" name="format-detection" />
+                    <meta content="yes" name="mobile-web-app-capable" />
+                    <meta content="#FFFFFF" name="theme-color" />
+                    <meta
+                        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+                        name="viewport"
+                    />
 
-                    <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
-                    <link rel="manifest" href="/manifest.json" />
-                    <link rel="shortcut icon" href="/favicon.ico" />
-                    <style>{`
-            html, body, #__next {
-              height: 100%;
-            }
-            #__next {
-              margin: 0 auto;
-            }
-            h1 {
-              text-align: center;
-            }
-            `}</style>
+                    <link href="/icons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+                    <link href="/manifest.json" rel="manifest" />
+                    <link href="/favicon.ico" rel="shortcut icon" />
                 </Head>
                 <body>
                     <Main />
