5710c0a072b9121e279a1db8bef8b65b259d5c85
Brody Dingel <brodydingel795@gmail.com>
2021-09-14 20:14:57 -0500

    Update worker index

--- worker/index.js
+++ worker/index.js
@@ -1,49 +1,52 @@
-"use strict";
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable no-undef */
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable no-restricted-globals */
 
-self.addEventListener("push", function (event) {
-	const data = JSON.parse(event.data.text());
-	event.waitUntil(
-		registration.showNotification(data.title, {
-			body: data.message,
-			icon: "/icons/android-chrome-192x192.png",
-		})
-	);
+self.addEventListener('push', (event) => {
+    const data = JSON.parse(event.data.text());
+
+    event.waitUntil(
+        registration.showNotification(data.title, {
+            body: data.message,
+            icon: '/icons/android-chrome-192x192.png'
+        })
+    );
 });
 
-self.addEventListener("notificationclick", function (event) {
-	event.notification.close();
-	event.waitUntil(
-		clients
-			.matchAll({ type: "window", includeUncontrolled: true })
-			.then(function (clientList) {
-				if (clientList.length > 0) {
-					let client = clientList[0];
-					for (let i = 0; i < clientList.length; i++) {
-						if (clientList[i].focused) {
-							client = clientList[i];
-						}
-					}
-					return client.focus();
-				}
-				return clients.openWindow("/");
-			})
-	);
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients
+            .matchAll({
+                includeUncontrolled: true,
+                type: 'window'
+            })
+            .then((clientList) => {
+                if (clientList.length) {
+                    let client = clientList[0];
+
+                    for (const element of clientList) {
+                        if (element.focused) {
+                            client = element;
+                        }
+                    }
+
+                    return client.focus();
+                }
+
+                return clients.openWindow('/');
+            })
+    );
 });
 
-// this is what allows you to send pushes even after the browser tab closes
-self.addEventListener("pushsubscriptionchange", function (event) {
-	event.waitUntil(
-		Promise.all([
-			Promise.resolve(
-				event.oldSubscription ? deleteSubscription(event.oldSubscription) : true
-			),
-			Promise.resolve(
-				event.newSubscription
-					? event.newSubscription
-					: subscribePush(registration)
-			).then(function (sub) {
-				return saveSubscription(sub);
-			}),
-		])
-	);
+self.addEventListener('pushsubscriptionchange', (event) => {
+    event.waitUntil(
+        Promise.all([
+            Promise.resolve(event.oldSubscription ? deleteSubscription(event.oldSubscription) : true),
+            Promise.resolve(event.newSubscription ? event.newSubscription : subscribePush(registration)).then((sub) =>
+                saveSubscription(sub)
+            )
+        ])
+    );
 });
