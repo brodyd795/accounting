827e24c81e3c3bb53bc3a1fe0fb516f947949c94
Brody Dingel <brodydingel795@gmail.com>
2021-09-18 06:37:36 -0500

    Summary table skeleton

--- components/home/accounts-summary-table.js
+++ components/home/accounts-summary-table.js
@@ -66,6 +66,40 @@ const StyledButton = styled.button`
     font-size: 16px;
 `;
 
+const SkeletonTd = styled.td`
+    height: 24px;
+`;
+
+const SkeletonTdContent = styled.div`
+    height: 100%;
+
+    box-shadow: 0 4px 10px 0 rgba(33, 33, 33, 0.15);
+    border-radius: 4px;
+    position: relative;
+    overflow: hidden;
+
+    ::before {
+        content: '';
+        display: block;
+        position: absolute;
+        left: -150px;
+        top: 0;
+        height: 100%;
+        width: 150px;
+        background: linear-gradient(to right, transparent 0%, #d8d8d8 50%, transparent 100%);
+        animation: load 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+    }
+
+    @keyframes load {
+        from {
+            left: -150px;
+        }
+        to {
+            left: 100%;
+        }
+    }
+`;
+
 const Row = ({account}) => {
     const {isDemo} = useDemo();
     const category = cleanAccountNameOrCategoryForUI(account.category);
@@ -84,6 +118,16 @@ const Row = ({account}) => {
     );
 };
 
+const RowSkeleton = () => (
+    <tr>
+        {Array.from({length: 3}, () => (
+            <SkeletonTd style={{padding: 0}}>
+                <SkeletonTdContent />
+            </SkeletonTd>
+        ))}
+    </tr>
+);
+
 export const AccountsSummaryTable = () => {
     const [selectedMonth, setSelectedMonth] = useState(new Date());
     const [shouldShowModal, setShouldShowModal] = useState(false);
@@ -94,7 +138,48 @@ export const AccountsSummaryTable = () => {
     }
 
     if (!data) {
-        return <div>{'Loading accounts summary...'}</div>;
+        return (
+            <StyledSummaryTableContainer>
+                <StyledH2>{'Accounts Summary'}</StyledH2>
+                <StyledTopRow>
+                    <StyledDatePicker
+                        dateFormat={'MMMM yyyy'}
+                        maxDate={new Date()}
+                        minDate={new Date(2021, 3, 1)}
+                        selected={new Date()}
+                    />
+                    <StyledButton type={'button'}>{'New transaction'}</StyledButton>
+                </StyledTopRow>
+                <StyledTablesContainer>
+                    <StyledTable>
+                        <thead>
+                            <tr>
+                                <th>{'Category'}</th>
+                                <th>{'Account Name'}</th>
+                                <th>{'Balance'}</th>
+                            </tr>
+                        </thead>
+                        {Array.from({length: 6}, () => (
+                            <RowSkeleton />
+                        ))}
+                    </StyledTable>
+                    <StyledTable>
+                        <thead>
+                            <tr>
+                                <th>{'Category'}</th>
+                                <th>{'Account Name'}</th>
+                                <th>{'Balance'}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {Array.from({length: 7}, () => (
+                                <RowSkeleton />
+                            ))}
+                        </tbody>
+                    </StyledTable>
+                </StyledTablesContainer>
+            </StyledSummaryTableContainer>
+        );
     }
 
     const monthlyAccounts = data.filter((account) => account.category === 'Income' || account.category === 'Expenses');
