4cb64e23cb8cb137672557424dcf2161a7f26a2e
Brody Dingel <brodydingel795@gmail.com>
2021-09-24 10:35:46 -0500

    Get rid of FOUC

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import Document, {Html, Head, Main, NextScript} from 'next/document';
+import {ServerStyleSheet} from 'styled-components';
 
 const APP_NAME = 'Dingel accounting';
 const APP_DESCRIPTION = "Our family's personal accounting system";
 
-export default class extends Document {
-    static getInitialProps(ctx) {
-        return Document.getInitialProps(ctx);
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
+
+        try {
+            // eslint-disable-next-line no-param-reassign
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                )
+            };
+        } finally {
+            sheet.seal();
+        }
     }
 
     render() {
